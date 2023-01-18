64f6ec30a0f7ac0d103a2530681388d13f3dc43c
Marian Lorinc <mlorinc@redhat.com>
2023-01-18 11:44:46 +0100

    FUSETOOLS2-1880 - Move 'Repeat' feature from DAP project
    Signed-off-by: Marian Lorinc <mlorinc@redhat.com>

--- out/conditions/Repeat.d.ts
+++ out/conditions/Repeat.d.ts
@@ -1,8 +1,5 @@
+import { TimeoutError } from './TimeoutPromise';
 export interface RepeatArguments {
-    /**
-     * Repeat function <count> times. If undefined function will be repeated infinitely or until time outs.
-     */
-    count?: number;
     /**
      * Repeat timeout after promise is rejected. If undefined function will be repeated <count> times or infinitely.
      */
@@ -14,14 +11,149 @@ export interface RepeatArguments {
     /**
      * Error message when repeat time outs.
      */
-    message?: string;
-    log?: boolean;
+    message?: string | (() => string | PromiseLike<string>);
     id?: string;
+    /**
+     * In case of timeout = 0 and unsuccessful repeat operation the promise will be resolved
+     * to undefined instead of throwing RepeatUnsuccessfulException.
+     */
+    ignoreLoopError?: boolean;
+}
+export declare class Threshold {
+    private start;
+    private interval;
+    private resetCounter;
+    constructor(interval: number);
+    reset(): void;
+    hasFinished(): boolean;
+    get resetCount(): number;
+}
+export declare enum LoopStatus {
+    LOOP_DONE = 0,
+    LOOP_UNDONE = 1
+}
+export declare type RepeatLoopResult<T> = {
+    value?: T;
+    loopStatus: LoopStatus;
+    delay?: number;
+};
+export declare class RepeatError extends Error {
+}
+export declare class RepeatExitError extends Error {
+}
+export declare class RepeatUnsuccessfulException extends TimeoutError {
 }
 /**
- * Repeat function until it returns truthy value.
+ * Repeat class was heavily inspired by Selenium WebDriver.wait method.
+ * However it was not possible to use in [@theia-extension-tester/abstract-element](https://www.npmjs.com/package/@theia-extension-tester/abstract-element)
+ * due to recursion problem when searching elements. By the time the functionality was
+ * extended even further and the repeat function was prioritized more than driver.wait function.
+ * Primarily to emulate [vscode-extension-tester](https://www.npmjs.com/package/vscode-extension-tester) behavior
+ * timeout usage was changed. All about that and other features will be described further in next paragraphs.
+ *
+ * The Repeat class implements the same features as driver.wait but there are some key differences:
+ *  1. Method behavior when timeout = 0 :: driver.wait uses infinity timeout instead / the Repeat class performs single iteration
+ *  2. Method behavior when timeout = undefined :: The same behavior. Loop indefinitely.
+ *  3. The Repeat class supports threshold timer. Timer which does not resolve promise immediately but checks
+ *  returned value was truthful for duration of threshold time. Useful when some page object is laggy.
+ *
+ *  Single loop in the class is one execution of given function as argument. This behavior can be overridden
+ *  by returning object in the given function. The object has the following format:
+ *  ```ts
+ *  {
+ *      // Value to be checked and returned.
+ *      value?: T;
+ *      // Use one of these to mark loop end.
+ *      // If given LOOP_DONE and timeout = 0 then the repeat returns the value.
+ *      loopStatus: LoopStatus.LOOP_DONE | LoopStatus.LOOP_UNDONE
+ *      // Delay next loop. Default is 0.
+ *      delay?: number;
+ *  }
+ * ```
+ *
+ *  Support for delaying next iteration. Some operations requires multiple checks but the checks cannot be repeated rapidly
+ *  in consecutive manner. This feature was for example used in TextEditor page object. Without delays Eclipse Che web sockets
+ *  were failing. To see how to use delays please refer to example above.
+ *
+ * Verify Input page object does not have error message when given input.
+ * This action is sometimes flaky and therefore repeat might be good for this situation.
+ * @example
+ * const input = await InputBox.create();
+ * // Do not await on purpose of demonstration.
+ * input.setText("hello@world.com");
+ * // Verify email validation went through.
+ * await repeat(async () => {
+ *  try {
+ *      // In case the input becomes stale.
+ *      const in = await InputBox.create();
+ *      return await in.hasError() === false;
+ *      // or
+ *      // return {
+ *      //   value: await in.hasError() === false,
+ *      //   loopStatus: LoopStatus.LOOP_DONE,
+ *      //   delay: 150
+ *      // };
+ *  }
+ *  catch (e) {
+ *      return false;
+ *  }
+ * }, {
+ *  timeout: 30000,
+ *  message: "Email could not be successfully verified.",
+ *  // Make sure the result is stable.
+ *  threshold: 1000
+ * })
+ */
+export declare class Repeat<T> {
+    protected func: (() => T | PromiseLike<T> | RepeatLoopResult<T> | PromiseLike<RepeatLoopResult<T>>);
+    protected options?: RepeatArguments | undefined;
+    protected timeout?: number;
+    protected id: string;
+    protected threshold: Threshold;
+    private message?;
+    private clearTask?;
+    private resolve;
+    private reject;
+    private promise;
+    private run;
+    private hasStarted;
+    private finishedLoop;
+    private usingExplicitLoopSignaling;
+    constructor(func: (() => T | PromiseLike<T> | RepeatLoopResult<T> | PromiseLike<RepeatLoopResult<T>>), options?: RepeatArguments | undefined);
+    /**
+     * Perform single loop of the task.
+     */
+    protected loop(): Promise<void>;
+    /**
+     * Execute repeat task.
+     * @returns A task result.
+     */
+    execute(): Promise<T>;
+    /**
+     * Abort repeat task. This function does not return anything but it makes the repeat function
+     * return the given value.
+     * @param value
+     *  Error | undefined :: Abort task and make repeat function return rejected promise.
+     *  T :: Abort task but make repeat function return resolved promise with the given value.
+     */
+    abort(value: Error | T | undefined): void;
+    /**
+     * Cleanup all timers and setImmediate handlers.
+     */
+    protected cleanup(): void;
+    /**
+     * Schedule next loop. If delay is set  then use setTimeout instead of setImmediate.
+     * @param delay Minimum time in ms until next iteration is executed.
+     */
+    private scheduleNextLoop;
+}
+/**
+ * Repeat function until it returns truthy value. For more information
+ * please see {@link Repeat}.
  *
  * @param func function to repeat
  * @param options repeat options
+ * @returns output value of the {@link func} function.
  */
-export declare function repeat<T>(func: (() => T | PromiseLike<T>), options?: RepeatArguments): Promise<T>;
+export declare function repeat<T>(func: (() => T | PromiseLike<T> | RepeatLoopResult<T> | PromiseLike<RepeatLoopResult<T>>), options?: RepeatArguments): Promise<T>;
+export { TimeoutError };
--- out/conditions/TimeoutPromise.js
+++ out/conditions/TimeoutPromise.js
@@ -10,75 +10,137 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.TimeoutPromise = exports.TimeoutError = void 0;
+function captureStackTrace(limit = 50) {
+    const temp = Error.stackTraceLimit;
+    try {
+        Error.stackTraceLimit = limit;
+        // get call stack
+        let callStack = {};
+        Error.captureStackTrace(callStack, captureStackTrace);
+        return callStack.stack;
+    }
+    finally {
+        Error.stackTraceLimit = temp;
+    }
+}
 /**
- * Error representing timed out promise.
+ * Error signaling timeout of assigned promise.
  */
 class TimeoutError extends Error {
-    constructor(message, callStack, id) {
+    /**
+     * Create new TimeoutError.
+     * @param owner Owner of the error. Currently promises are identified by resolve functions.
+     * @param message Base message to be shown.
+     * @param callStack Call stack log to be included as part of the error.
+     * @param id Promise debug identification. Arbitrary value for debugging purposes.
+     */
+    constructor(owner, message, callStack, id) {
         if (message) {
-            super(message + `\n${callStack}`);
+            super(`Promise(id=${id}) ${message}\r\n\r\n${callStack}`);
         }
         else {
-            super(`Promise(id=${id}) timed out after 1 cycle (zero timeout).\n${callStack}`);
+            super(`Promise(id=${id}) timed out after 1 cycle (zero timeout).\r\n\r\n$${callStack}`);
         }
+        this.owner = owner;
         this.name = 'TimeoutError';
+        this.setMessageIndex();
+    }
+    setMessageIndex() {
+        this.messageIndex = this.message.indexOf('\r\n\r\n') + '\r\n\r\n'.length;
+    }
+    /**
+     * Check if some promise has thrown this error.
+     * @param obj Resolve function of the promise.
+     * @returns Boolean value according if the promise has thrown the error.
+     */
+    isThrownBy(obj) {
+        return this.owner === obj;
+    }
+    /**
+     * Prepend additional call stack to current call stack.
+     * @param callStack Call stack to be prepended.
+     */
+    prependCallStack(callStack) {
+        this.message = `${this.message.substring(0, this.messageIndex)}${callStack}${this.message.substring(this.messageIndex)}`;
+    }
+    /**
+     * Append error message to existing error message.
+     * @param message Error message to be appended.
+     */
+    appendMessage(message) {
+        const endMessageIndex = this.messageIndex - '\r\n\r\n'.length;
+        this.message = `${this.message.substring(0, endMessageIndex)}${message}${this.message.substring(this.messageIndex)}`;
+        this.setMessageIndex();
     }
 }
 exports.TimeoutError = TimeoutError;
 /**
- * Object representing promise with timeout.
+ * Class with single static method and a constructor to create new TimeoutPromise instance.
+ * When timeout is reached, the promise is rejected with {@link TimeoutError}.
+ *
+ * @example
+ * // Create new TimeoutPromise with Executor.
+ * new TimeoutPromise((resolve) => resolve(5), 5000, {id: "Return 5", message: "Could not return 5."});
+ * @example
+ * // Crate new TimeoutPromise from existing promise.
+ * TimeoutPromise.createFrom(returnFivePromise, 5000, {id: "Return 5", message: "Could not return 5."});
  */
 class TimeoutPromise extends Promise {
     /**
-     *
-     * @param executor promise executor (resolve, reject) => ...;
-     * @param timeout promise timeout. If timeout value is zero or undefined, timeout is infinite.
-     * @param options promise options
+     * Create new TimeoutPromise.
+     * @param executor Executor functions resolve and reject known from promises.
+     * @param timeout Timeout in ms.
+     * @param options Additional {@link TimeoutPromiseOptions}.
+     * @example
+     * // Create new TimeoutPromise with Executor.
+     * new TimeoutPromise((resolve) => resolve(5), 5000, {id: "Return 5", message: "Could not return 5."});
      */
     constructor(executor, timeout, options) {
         super(TimeoutPromise.create(executor, timeout, options));
     }
-    static decorateResolver(resolver, timer) {
+    /**
+     * Decorate resolve and reject functions to clear timer. Reject is decorated to add call stack if it was reason of
+     * TimeoutError.
+     * @param resolver Either resolve or reject function.
+     * @param timer NodeJS.Timeout object assigned to this promise.
+     * @param resolve In this case resolve function is used as promise id.
+     * @param callStack Additional information about call stack.
+     * @returns Decorated resolve / reject function.
+     */
+    static decorateResolver(resolver, timer, resolve, callStack) {
         return (value) => {
             if (timer) {
                 clearTimeout(timer);
+            }
+            // in case of reject prepend additional call stack
+            if (resolve && (value instanceof TimeoutError) && !value.isThrownBy(resolve)) {
+                value.prependCallStack(callStack);
             }
             resolver(value);
         };
     }
     static create(executor, timeout, options) {
+        var _a;
+        // get call stack
+        const callStack = (_a = options === null || options === void 0 ? void 0 : options.callStack) !== null && _a !== void 0 ? _a : captureStackTrace();
         return (resolve, reject) => __awaiter(this, void 0, void 0, function* () {
             let timer = null;
             const id = (options === null || options === void 0 ? void 0 : options.id) || "anonymous";
             if (typeof timeout === 'number' && timeout < 0) {
                 throw new Error('Timeout cannot be negative.');
             }
-            if (timeout !== undefined && timeout !== 0) {
+            if (timeout !== undefined) {
                 const start = Date.now();
-                // get call stack
-                let callStack = options === null || options === void 0 ? void 0 : options.callStack;
-                try {
-                    if (callStack === undefined) {
-                        throw new Error();
-                    }
-                }
-                catch (e) {
-                    callStack = e.stack;
-                    callStack = callStack === null || callStack === void 0 ? void 0 : callStack.split('\n\t').join('\t\n');
-                }
-                timer = setTimeout(() => {
+                timer = setTimeout(() => __awaiter(this, void 0, void 0, function* () {
                     let message = `Promise(id=${id}) timed out after ${Date.now() - start}ms.`;
                     if (options === null || options === void 0 ? void 0 : options.message) {
-                        message += ` Reason: ${options.message}`;
+                        message += ` Reason: ${options.message instanceof Function ? yield options.message() : options.message}`;
                     }
-                    reject(new TimeoutError(message, callStack, id));
-                    if (options === null || options === void 0 ? void 0 : options.onTimeout) {
-                        options === null || options === void 0 ? void 0 : options.onTimeout();
-                    }
-                }, timeout);
+                    reject(new TimeoutError(resolve, message, callStack, id));
+                }), timeout);
             }
             resolve = this.decorateResolver(resolve, timer);
-            reject = this.decorateResolver(reject, timer);
+            reject = this.decorateResolver(reject, timer, resolve, callStack);
             try {
                 executor(resolve, reject);
             }
@@ -86,6 +148,22 @@ class TimeoutPromise extends Promise {
                 reject(e);
             }
         });
+    }
+    /**
+     * Create new TimeoutPromise from existing promise.
+     * @param promise Existing promise to be wrapped.
+     * @param timeout Timeout in ms.
+     * @param options Additional information about call stack.
+     * @returns New TimeoutPromise.
+     * @example
+     * // Crate new TimeoutPromise from existing promise.
+     * TimeoutPromise.createFrom(returnFivePromise, 5000, {id: "Return 5", message: "Could not return 5."});
+     */
+    static createFrom(promise, timeout, options) {
+        return new TimeoutPromise((resolve, reject) => {
+            promise.then(resolve);
+            promise.catch(reject);
+        }, timeout, options);
     }
 }
 exports.TimeoutPromise = TimeoutPromise;
