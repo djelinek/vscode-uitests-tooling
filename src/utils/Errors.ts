8f0b5df536b506f65c201e34ccad8a66f16d1df4
Marian Lorinc <mlorinc@redhat.com>
2023-03-02 16:35:06 +0100

    FUSETOOLS2-1965 Add ignore error option to repeat
    Signed-off-by: Marian Lorinc <mlorinc@redhat.com>

---
+++ src/utils/Errors.ts
@@ -0,0 +1,18 @@
+import { error } from "vscode-extension-tester";
+
+export type ErrorType = Function;
+
+/**
+ * List of errors that are usually responsible for WebElement interactivity flakyness. 
+ */
+export const INTERACTIVITY_ERRORS: ErrorType[] = [error.StaleElementReferenceError, error.NoSuchElementError, error.ElementNotInteractableError];
+
+/**
+ * Check whether thrown error is in the list.
+ * @param e thrown error
+ * @param errors errors to be checked
+ * @returns true if e is in error list, otherwise false
+ */
+export function is(e: any, ...errors: ErrorType[]): boolean {
+	return errors.find((err) => e instanceof err) !== undefined;
+}
