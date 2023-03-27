0cf7d913e62ff26deaf1e0aa9489ccd8cc153e92
Marian Lorinc <mlorinc@redhat.com>
2023-03-27 19:29:19 +0200

    FUSETOOLS2-1984 Export vscode-extension-tester
    Signed-off-by: Marian Lorinc <mlorinc@redhat.com>

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 'use strict';
 
+export * from 'vscode-extension-tester';
+
 /**
  * components
  */
-export * from './components/activityBar/ActivityBar';
+export { ActivityBar } from './components/activityBar/ActivityBar';
 
-export * from './components/bottomBar/OutputView';
+export { OutputView } from './components/bottomBar/OutputView';
 
 export * from './components/custom/CommandPalette';
 export * from './components/custom/Marketplace';
@@ -14,14 +16,14 @@ export * from './components/custom/Project';
 export * from './components/dialog/InputBoxOpenDialogs';
 export * from './components/dialog/IOpenDialog';
 
-export * from './components/editor/ContentAssist';
-export * from './components/editor/TextEditor';
+export { ContentAssist } from './components/editor/ContentAssist';
+export { TextEditor } from './components/editor/TextEditor';
 
-export * from './components/statusBar/StatusBar';
+export { StatusBar } from './components/statusBar/StatusBar';
 
-export * from './components/workbench/Input';
+export { Input } from './components/workbench/Input';
 export * from './components/workbench/NotificationCenter';
-export * from './components/workbench/Workbench';
+export { Workbench } from './components/workbench/Workbench';
 
 /**
  * conditions
@@ -44,6 +46,5 @@ export * from './conditions/WaitUntil';
  */
 export * from './utils/FileSystem';
 export * from './utils/LogAnalyzer';
-export * from './utils/Mocha';
 export * from './utils/PathUtils';
 export * as errors from './utils/Errors';
