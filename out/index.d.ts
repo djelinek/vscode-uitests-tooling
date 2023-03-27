7651f6a493e3f952457ea52cd221363eaa9e0a6a
Marian Lorinc <mlorinc@redhat.com>
2023-03-27 19:29:19 +0200

    FUSETOOLS2-1984 Export vscode-extension-tester
    Signed-off-by: Marian Lorinc <mlorinc@redhat.com>

--- out/index.d.ts
+++ out/index.d.ts
@@ -1,19 +1,20 @@
+export * from 'vscode-extension-tester';
 /**
  * components
  */
-export * from './components/activityBar/ActivityBar';
-export * from './components/bottomBar/OutputView';
+export { ActivityBar } from './components/activityBar/ActivityBar';
+export { OutputView } from './components/bottomBar/OutputView';
 export * from './components/custom/CommandPalette';
 export * from './components/custom/Marketplace';
 export * from './components/custom/Project';
 export * from './components/dialog/InputBoxOpenDialogs';
 export * from './components/dialog/IOpenDialog';
-export * from './components/editor/ContentAssist';
-export * from './components/editor/TextEditor';
-export * from './components/statusBar/StatusBar';
-export * from './components/workbench/Input';
+export { ContentAssist } from './components/editor/ContentAssist';
+export { TextEditor } from './components/editor/TextEditor';
+export { StatusBar } from './components/statusBar/StatusBar';
+export { Input } from './components/workbench/Input';
 export * from './components/workbench/NotificationCenter';
-export * from './components/workbench/Workbench';
+export { Workbench } from './components/workbench/Workbench';
 /**
  * conditions
  */
@@ -30,6 +31,5 @@ export * from './conditions/WaitUntil';
  */
 export * from './utils/FileSystem';
 export * from './utils/LogAnalyzer';
-export * from './utils/Mocha';
 export * from './utils/PathUtils';
 export * as errors from './utils/Errors';
--- out/index.js
+++ out/index.js
@@ -14,23 +14,31 @@ var __exportStar = (this && this.__exportStar) || function(m, exports) {
     for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.errors = void 0;
+exports.errors = exports.Workbench = exports.Input = exports.StatusBar = exports.TextEditor = exports.ContentAssist = exports.OutputView = exports.ActivityBar = void 0;
+__exportStar(require("vscode-extension-tester"), exports);
 /**
  * components
  */
-__exportStar(require("./components/activityBar/ActivityBar"), exports);
-__exportStar(require("./components/bottomBar/OutputView"), exports);
+var ActivityBar_1 = require("./components/activityBar/ActivityBar");
+Object.defineProperty(exports, "ActivityBar", { enumerable: true, get: function () { return ActivityBar_1.ActivityBar; } });
+var OutputView_1 = require("./components/bottomBar/OutputView");
+Object.defineProperty(exports, "OutputView", { enumerable: true, get: function () { return OutputView_1.OutputView; } });
 __exportStar(require("./components/custom/CommandPalette"), exports);
 __exportStar(require("./components/custom/Marketplace"), exports);
 __exportStar(require("./components/custom/Project"), exports);
 __exportStar(require("./components/dialog/InputBoxOpenDialogs"), exports);
 __exportStar(require("./components/dialog/IOpenDialog"), exports);
-__exportStar(require("./components/editor/ContentAssist"), exports);
-__exportStar(require("./components/editor/TextEditor"), exports);
-__exportStar(require("./components/statusBar/StatusBar"), exports);
-__exportStar(require("./components/workbench/Input"), exports);
+var ContentAssist_1 = require("./components/editor/ContentAssist");
+Object.defineProperty(exports, "ContentAssist", { enumerable: true, get: function () { return ContentAssist_1.ContentAssist; } });
+var TextEditor_1 = require("./components/editor/TextEditor");
+Object.defineProperty(exports, "TextEditor", { enumerable: true, get: function () { return TextEditor_1.TextEditor; } });
+var StatusBar_1 = require("./components/statusBar/StatusBar");
+Object.defineProperty(exports, "StatusBar", { enumerable: true, get: function () { return StatusBar_1.StatusBar; } });
+var Input_1 = require("./components/workbench/Input");
+Object.defineProperty(exports, "Input", { enumerable: true, get: function () { return Input_1.Input; } });
 __exportStar(require("./components/workbench/NotificationCenter"), exports);
-__exportStar(require("./components/workbench/Workbench"), exports);
+var Workbench_1 = require("./components/workbench/Workbench");
+Object.defineProperty(exports, "Workbench", { enumerable: true, get: function () { return Workbench_1.Workbench; } });
 /**
  * conditions
  */
@@ -50,7 +58,6 @@ __exportStar(require("./conditions/WaitUntil"), exports);
  */
 __exportStar(require("./utils/FileSystem"), exports);
 __exportStar(require("./utils/LogAnalyzer"), exports);
-__exportStar(require("./utils/Mocha"), exports);
 __exportStar(require("./utils/PathUtils"), exports);
 exports.errors = require("./utils/Errors");
 //# sourceMappingURL=index.js.map
